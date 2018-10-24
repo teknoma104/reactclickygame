70c0d771d3dc6f492617e079708d76b8fa4cf896
Tommy Le <teknoma104@gmail.com>
2018-10-24 04:27:19 -0500

    Adjusted game logic so that the user has to click through all 12 unique character portraits without any duplicate image clicks in order to win

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import "./App.css";
 class App extends Component {
     // Setting this.state.friends to the friends json array
     state = {
-        currentCharacters: friends,
-        lastPicked: "",
+        charactersClicked: [],
+        currentCharacters: [],
         score: 0,
-        topScore: 0
+        topScore: 0,
+        answer: "neutral"
     };
 
     componentDidMount() {
+        console.log("mounting on load");
         this.selectTwelve();
     }
 
@@ -29,137 +31,122 @@ class App extends Component {
         const min = 1;
         const max = 20;
 
-        if (!this.state.lastPicked) {
-            console.log("no character last picked, picking 12 random characters");
-            console.log("confirming this.state.lastPicked:  " + this.state.lastPicked);
+        // Do while loop to get random 12 characters from the character roster (20 characters)
+        do {
+            // Variable to store a randomly generated number between 1-20
+            let rand = Math.floor(Math.random() * (max - min) + min);
+            // console.log("random number = " + rand);
 
-            do {
-                let rand = Math.floor(Math.random() * (max - min) + min);
-                // console.log("random number = " + rand);
-
-                if (rngNumbers.length === 12) {
-                    foundTwelve = true;
-                }
-                else if (rngNumbers.indexOf(rand) < 0) {
-                    // console.log("else condition");
-                    // console.log("rngNumbers.indexOf(" + rand + ") < 0:  " + rngNumbers.indexOf(rand));
-                    rngNumbers.push(rand);
-                }
-
-            } while (!foundTwelve);
-
-            console.log("testing rngNumbers array");
-            console.log(rngNumbers);
-
-            for (let x = 0; x < rngNumbers.length; x++) {
-                // console.log("iteration #" + x);
-
-                // Subtract one because the ID in friends.json begins with 1-20 although the array counts as 0-19
-                // That way when we loop through rngNumbers[x] to get the character ID we want from friends, their actual array index is 1 less than their ID property
-                randomCharacters.push(friends[rngNumbers[x] - 1]);
+            // if array is 12 in length, set foundTwelve to true to break the do..while loop
+            // else if the randomly generated number between 1-20 is not found in the the array, add it to the array
+            if (rngNumbers.length === 12) {
+                foundTwelve = true;
+            }
+            else if (rngNumbers.indexOf(rand) < 0) {
+                rngNumbers.push(rand);
             }
 
-            this.setState({ currentCharacters: randomCharacters });
+        } while (!foundTwelve);
 
-        } else if (this.state.lastPicked) {
-            console.log("character clicked before, adding that + picking 11 random characters");
-            console.log("testing this.state.lastPicked:  " + this.state.lastPicked);
+        // Uses the array of 12 RNG numbers above to now get the characters from the roster and store it into a new array of objects
+        for (let x = 0; x < rngNumbers.length; x++) {
 
-            rngNumbers.push(this.state.lastPicked);
-            console.log("testing rngNumbers array after adding the lastPicked char to array");
-            console.log(rngNumbers);
-
-            do {
-                let rand = Math.floor(Math.random() * (max - min) + min);
-                // console.log("random number = " + rand);
-
-                if (rngNumbers.length === 12) {
-                    foundTwelve = true;
-                }
-                else if (rngNumbers.indexOf(rand) < 0) {
-                    // console.log("else condition");
-                    // console.log("rngNumbers.indexOf(" + rand + ") < 0:  " + rngNumbers.indexOf(rand));
-                    rngNumbers.push(rand);
-                }
-
-            } while (!foundTwelve);
-
-            console.log("testing rngNumbers array");
-            console.log(rngNumbers);
-
-            for (let x = 0; x < rngNumbers.length; x++) {
-                // console.log("iteration #" + x);
-                // console.log("testing rngNumbers["+x+"]:  " + rngNumbers[x] );
-                // console.log("testing friends[rngNumbers["+x+"]]:  ");
-                // console.log(friends[rngNumbers[x]-1]);
-
-                // Subtract one because the ID in friends.json begins with 1-20 although the array counts as 0-19
-                // That way when we loop through rngNumbers[x] to get the character ID we want from friends, their actual array index is 1 less than their ID property
-                randomCharacters.push(friends[rngNumbers[x] - 1]);
-            }
-
-            console.log("testing randomCharacters");
-            console.log(randomCharacters);
-
-
-            this.setState({ currentCharacters: randomCharacters });
+            // Subtract one because the ID in friends.json begins with 1-20 but accessing the array requires starting 0-19
+            // That way when we loop through rngNumbers[x] to get the character ID we want from friends, their actual array index is 1 less than their ID property
+            randomCharacters.push(friends[rngNumbers[x] - 1]);
         }
+
+        this.setState({ currentCharacters: randomCharacters });
 
     }
 
+    shuffleCharacters = () => {
+        console.log("Shuffle function called");
+        
+        let array = this.state.currentCharacters;
+        console.log("testing currentCharacters array - pre shuffled");
+        console.log(array);
+
+        // Using Durstenfield shuffle, a computer-optimized version of Fisher-Yates
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+        }
+
+        console.log("testing currentCharacters array - after shuffled");
+        console.log(array);
+
+        this.setState({ currentCharacters: array });
+    }
+
     clickCharacter = id => {
-        // Destructure the name and value properties off of event.target
-        // Update the appropriate state
+        const newTopScore = this.state.score;
+
         console.log("testing id clicked:  " + id);
 
-        // if the clicked id matches the id in state's lastPicked, game is over
-        if (id === this.state.lastPicked) {
-            console.log("you clicked this character already, game over");
-            console.log("resetting state")
+        // if the clicked character's ID can be found in the charactersClicked array via indexOf(possible value of 0 and above)
+        // then the game is over
+        if (this.state.charactersClicked.indexOf(id) >= 0) {
+            console.log("character id#" + id + " has been clicked already");
+            console.log("resetting score");
 
-            // if current score is higher than top score, replace top score with current score
-            // if not, just reset current score to 0 and reset lastPicked to empty string
+            // if current score is higher than top score, replace top score with current score and reset current score to 0 
+            // if not, only reset current score to 0 
+            // both conditions should reset charactersClicked back to empty array
             if (this.state.score > this.state.topScore) {
-                console.log("current score is higher than topScore");
-                const newTopScore = this.state.score;
+                console.log("current score is higher than topScore");        
 
-                this.setState({ lastPicked: "", score: 0, topScore: newTopScore });
+                this.setState({ charactersClicked: [], score: 0, topScore: newTopScore, answer: "incorrect" });
                 this.selectTwelve();
 
             } else {
-                this.setState({ lastPicked: "", score: 0 });
+                this.setState({ charactersClicked: [], score: 0, answer: "incorrect" });
                 this.selectTwelve();
             }
 
-        } else {
-            console.log("congrats you clicked a character you didn't pick in the last click");
-            console.log("adding 1 point to current score");
-            console.log("testing id clicked again:  " + id);
+        }
+        // if the clicked character's ID can not be found in the charactersClicked array,
+        // then user can keep progressing with the game that clicked character's ID will be pushed to state's charactersClicked
+        // and score will be incremented by 1 before we shuffle the currentCharacters array of 12 characters
+        else if (this.state.charactersClicked.indexOf(id) < 0) {
+            console.log("character id#" + id + " has not been clicked yet");
+            console.log("adding +1 score, adding clicked id to characterClicked array");
 
-            console.log("testing state");
-            console.log(this.state);
-            console.log("testing lastPicked state:  " + this.state.lastPicked);
-            // return this.setState(({ lastPicked, score }) => ({lastPicked: id, score: this.state.score + 1}));
-            this.setState((state) => { return { lastPicked: id, score: this.state.score + 1 } });
-            this.selectTwelve();
+            let clickedCharactersArray = this.state.charactersClicked;
+
+            console.log("testing array before push");
+            console.log(clickedCharactersArray)
+
+            clickedCharactersArray.push(id);
+
+            console.log("testing array after push");
+            console.log(clickedCharactersArray);
+
+            if (clickedCharactersArray.length === 12) {
+                console.log("User guessed all 12 characters without clicking a duplicate image, user wins");
+                this.setState({ charactersClicked: [], score: 0, topScore: newTopScore + 1, answer: "winner" });
+            }
+            else {
+                this.setState((state) => { return { charactersClicked: clickedCharactersArray, score: this.state.score + 1, answer: "correct" } });
+                this.shuffleCharacters();
+            }
         }
 
     };
-
-
 
     // Map over this.state.friends and render a FriendCard component for each friend object
     render() {
         // console.log("testing friends");
         // console.log(friends);
-        console.log("page refreshed");
-        console.log("test state");
+        console.log("page loaded");
+        console.log("checking state values");
         console.log(this.state);
         return (
             <div>
                 <Nav
                     score={this.state.score}
                     topScore={this.state.topScore}
+                    answer={this.state.answer}
                 />
                 <Header backgroundImage="/assets/images/SFIII3rdStrike_logo.png">
                     <h1>React Clicky Game - SFIII 3rd Strike edition!</h1>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,16 @@ const Nav = (props) => (
             <li className="brand">
                 <a href="/">Clicky Game</a>
             </li>
-            <li className="">Click an image to begin!</li>
+            {/* <InfoText state={props.answer} /> */}
+            {
+                {
+                    'neutral': <li className="infoText">Click an image to begin!</li>,
+                    'correct': <li className="infoText">You guessed correctly! Keep on going!</li>,
+                    'incorrect': <li className="infoText">You guessed incorrectly! Gane over!</li>,
+                    'winner': <li className="infoText">You guessed all 12 characters! You win!</li>
+                }[props.answer]
+            }
+            {/* <li className="infoText">Click an image to begin!</li> */}
             <li>Score: {props.score} | Top Score: {props.topScore}</li>
         </ul>
         {/* <a className="navbar-brand" href="/">
